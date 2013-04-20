0a1053701dc4cdf6ac5668bb3770caa6d683133a
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-04-20 11:05:27 +0300

    Enabling fruitstrap device testing

--- ios/run.sh
+++ ios/run.sh
@@ -8,7 +8,20 @@ fi
 if [ "x$target" = "xios-sim" ]; then
     "$IOS_PROJECT_PATH/cordova/run"
 elif [ "x$target" = "xios-dev" ]; then
+
     GDB_COMMANDS_FILE="/tmp/fruitstrap-gdb-commands"
+    FRUITSTRAP="$DOWNLOADS_PATH/fruitstrap/fruitstrap"
+
+    # Check if a device can be found
+    "$FRUITSTRAP" -c -t 2 > /dev/null || NO_DEVICE=YES
+    if [ "x$NO_DEVICE" = "xYES" ]; then
+        echo "No device found."
+        exit
+    fi
+
+    # Give time to previous fruitstrap instance to release the device.
+    sleep 1
+
     if [ "x$conf" = "xtesting" ]; then
         cat << EOF > "$GDB_COMMANDS_FILE"
 set confirm off
@@ -30,7 +43,7 @@ end
 continue
 EOF
         STDOUT="$PROJECT_PATH/build/stdout.txt"
-        "$DOWNLOADS_PATH/fruitstrap/fruitstrap" -b "$IOS_PROJECT_PATH/build/$PROJECT_NAME.app" -t 5 -d -x "$GDB_COMMANDS_FILE" > "$STDOUT" 2>&1 || error "Failed to install to device"
+        "$FRUITSTRAP" -b "$IOS_PROJECT_PATH/build/$PROJECT_NAME.app" -t 5 -d -x "$GDB_COMMANDS_FILE" > "$STDOUT" 2>&1 || error "Failed to install to device"
         result=`cat "$STDOUT" | grep QUnit.done`
         [ "x$result" = "x" ] && exit 1
         total=`echo $result | cut -d: -f2`
@@ -42,6 +55,6 @@ EOF
 
     else
         echo 'continue' > "$GDB_COMMANDS_FILE"
-        "$DOWNLOADS_PATH/fruitstrap/fruitstrap" -b "$IOS_PROJECT_PATH/build/$PROJECT_NAME.app" -t 5 -d -x "$GDB_COMMANDS_FILE" || error "Failed to install to device"
+        "$FRUITSTRAP" -b "$IOS_PROJECT_PATH/build/$PROJECT_NAME.app" -t 5 -d -x "$GDB_COMMANDS_FILE" || error "Failed to install to device"
     fi
 fi
