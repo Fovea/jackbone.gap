ea882fa632800856e80da1f56f67cb181468cf66
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-04-20 11:09:15 +0300

    Adding on-device iOS testing

--- tests/test-script.sh
+++ tests/test-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ "x$TESTNAME" = "x" ]; then
     exit 1;
 fi
@@ -26,3 +28,9 @@ test -e "$TESTFILE" && cp "$TESTFILE" app/js/appdelegate.js || echo "$TESTFILE" 
 
 # Run the test
 "$JB" run || exit 1
+
+# XCode found, assume we're developing for iOS
+if test -e /Developer/Applications/Xcode.app; then
+    "$JB" build ios-dev testing
+    "$JB" run
+fi
