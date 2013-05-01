c686cfd0b554f5c1d40ee85969a9be8e2c872dac
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-05-01 17:01:11 +0300

    Fixing lint

--- js/cdv.js
+++ js/cdv.js
@@ -1,13 +1,14 @@
 /**
  * @fileoverview Cordova Wrapper.
  */
+/* global cordova, Media */
 define([
-    'jquery',
-    'underscore',
-    'logger',
-    'testflight'
+    "jquery",
+    "underscore",
+    "logger",
+    "testflight"
 ], function ($, _, Logger) {
-    'use strict';
+    "use strict";
 
     /**
      * Sets Cordova initial configuration, checks for supported features, loads plugins.
@@ -16,43 +17,47 @@ define([
      * @constructor
      */
     var Cordova = {};
-    
+
     /** Initialize Cordova Plugins. */
-    Cordova.initialize = function() {
-        if (typeof cordova !== 'undefined') {
+    Cordova.initialize = function () {
+        if (typeof cordova !== "undefined") {
             try {
                 Cordova.testflight = cordova.require("cordova/plugin/testflightsdk");
             }
             catch (e) {
-                Logger.log('TestFlight not found');
+                Logger.log("TestFlight not found");
             }
-            if (typeof Cordova.testflight !== 'undefined') {
-                Cordova.testflight.setDeviceIdentifierUUID(function(){}, function(){});
+            if (typeof Cordova.testflight !== "undefined") {
+                Cordova.testflight.setDeviceIdentifierUUID(function () {}, function () {});
                 Cordova.testflight.takeOff(
-                    function() { /* Win */  },
-                    function() { /* Fail */ Cordova.testflight = false; },
+                    function () { /* Win */  },
+                    function () { /* Fail */ Cordova.testflight = false; },
                     "723945a1-4204-4c2a-82ac-63201cd6b20f"); // App Token
 
-                $(window).error(function(e) {
+                $(window).error(function (e) {
                     if (Cordova.testflight !== false) {
                         // var file = e.originalEvent.filename + ":" + e.originalEvent.lineno;
                         var line = e.originalEvent.lineno;
                         var msg  = e.originalEvent.message;
-                        var de = new Error('dummy');
-                        var stack = de.stack.replace(/^[^\(]+?[\n$]/gm, '')
-                            .replace(/^\s+at\s+/gm, '')
-                            .replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
-                            .split('\n');
-                        var decostack = 'Stacktrace:\n----------------\n' + stack + '\n----------------';
-                        Cordova.testflight.remoteLog(function(){}, function(){}, "ERROR line " + line + ": " + msg + '\n' + decostack);
+                        var de = new Error("dummy");
+                        var stack = de.stack.replace(/^[^\(]+?[\n$]/gm, "")
+                            .replace(/^\s+at\s+/gm, "")
+                            .replace(/^Object.<anonymous>\s*\(/gm, "{anonymous}()@")
+                            .split("\n");
+                        var decostack = "Stacktrace:\n----------------\n" + stack + "\n----------------";
+                        Cordova.testflight.remoteLog(function () {},
+                                                     function () {},
+                                                     "ERROR line " + line + ": " + msg + "\n" + decostack);
                     }
                 });
             }
-            else
+            else {
                 Cordova.testflight = false;
+            }
         }
-        else
+        else {
             Cordova.testflight = false;
+        }
 
         var ua = navigator.userAgent.toLowerCase();
         Cordova.isAndroid = ua.indexOf("android") > -1;
@@ -62,39 +67,45 @@ define([
     /** Log a checkpoint in the application.
      * @param checkpointName String describing the checkpoint.
      */
-    Cordova.passCheckpoint = function(checkpointName) {
+    Cordova.passCheckpoint = function (checkpointName) {
         // Logger.log("[CHECKPOINT] " + checkpointName);
-        if (Cordova.testflight !== false)
-            Cordova.testflight.passCheckpoint(function(){}, function(){}, checkpointName);
+        if (Cordova.testflight !== false) {
+            Cordova.testflight.passCheckpoint(function () {}, function () {}, checkpointName);
+        }
     };
 
     /** Perform a subtle notification, like a vibration if supported. */
-    Cordova.notify = function() {
-        if (navigator && navigator.notification && navigator.notification.vibrate)
+    Cordova.notify = function () {
+        if (navigator && navigator.notification && navigator.notification.vibrate) {
             navigator.notification.vibrate();
+        }
     };
 
     /** Show an alert dialog, with a title and a text. */
-    Cordova.alert = function(title, txt) {
-        if (navigator && navigator.notification && navigator.notification.alert)
-            navigator.notification.alert(txt, function(){}, title);
-        else
+    Cordova.alert = function (title, txt) {
+        if (navigator && navigator.notification && navigator.notification.alert) {
+            navigator.notification.alert(txt, function () {}, title);
+        }
+        else {
             alert(title + "\n" + txt);
+        }
     };
 
     /** Hide the native splash screen, if any. */
-    Cordova.hideNativeSplash = function() {
-        if (navigator && navigator.splashscreen && navigator.splashscreen.hide)
+    Cordova.hideNativeSplash = function () {
+        if (navigator && navigator.splashscreen && navigator.splashscreen.hide) {
             navigator.splashscreen.hide();
+        }
     };
 
     /** Log a message, sends remotely to TestFlight.
      * @param message Text to log.
      */
-    Cordova.log = function(message) {
+    Cordova.log = function (message) {
         Logger.log(message);
-        if (Cordova.testflight !== false && message)
-            Cordova.remoteLog(function(){}, function(){}, message);
+        if (Cordova.testflight !== false && message) {
+            Cordova.remoteLog(function () {}, function () {}, message);
+        }
     };
 
     var soundLoadedSuccess = function () {};
@@ -103,7 +114,7 @@ define([
     /** Play a sound.
      * @param url of the sound.
      */
-    Cordova.playSound = function(url) {
+    Cordova.playSound = function (url) {
         // If Media API is supported.
         if (window.Media) {
             if (!sounds[url]) {
