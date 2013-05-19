43fb91ecb68553e4ed87c01f325a895f20f45d6d
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-05-19 12:40:00 +0300

    Using high-entropy random number generator

--- js/dbstorage.js
+++ js/dbstorage.js
@@ -12,14 +12,34 @@
 define(["underscore", "backbone", "database"], function (_, Backbone, Database) {
     "use strict";
 
-    /** Generate four random hex digits. */
-    function S4() {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    var root = this;
+
+    // Generate four random hex digits.
+    // function S4() {
+    //     return (((1 + Math.random()) * 0x10000) | 0)rtoString(16).substring(1);
+    // }
+
+    // Fill an array of 16 bits random values.
+    var getRandomValues16;
+    var crypto = (root.crypto || {});
+    crypto.getRandomValues16 = (
+        crypto.getRandomValues ||
+        function (array) {
+            for (var i = 0; i < array.length; ++i) {
+                array[i] = (((1 + Math.random()) * 0x10000) | 0) - 0x10000;
+            }
+        }
+    );
+
+    function S4(array,i) {
+        return (array[i] + 0x10000).toString(16).substring(1);
     }
 
     /** Generate a pseudo-GUID by concatenating random hexadecimal. */
     function guid() {
-        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+        var a = new Uint16Array(8);
+        crypto.getRandomValues16(a);
+        return (S4(a,0) + S4(a,1) + "-" + S4(a,2) + "-" + S4(a,3) + "-" + S4(a,4) + "-" + S4(a,5) + S4(a,6) + S4(a,7));
     }
 
     /** Returns the array of values for a model.
