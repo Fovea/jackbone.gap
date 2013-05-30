1a5c4ac61674911a863dcbe9e8c58c80a0974710
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-05-30 13:41:57 +0300

    Support for transactions

--- js/database.js
+++ js/database.js
@@ -25,6 +25,9 @@ function (Logger, _, Events, Jackbone/*, SQLite*/) {
     /** Set to true to log all SQL requests */
     Database.loggingEnabled = false;
 
+    /** Request wether transactions (BEGIN/COMMIT) are supported */
+    Database.supportsTransactions = (window.sqlitePlugin ? true : false);
+
     /** Open a database from its name.
      *
      * @param name Database name
@@ -60,6 +63,132 @@ function (Logger, _, Events, Jackbone/*, SQLite*/) {
         });
     };
 
+    var webSQL_executeSql = function (tx, query, args, success, error, log, context) {
+        // Log the request
+        var sqlId, loggingEnabled = Database.loggingEnabled;
+        if (loggingEnabled && log !== false) {
+            sqlId = _.uniqueId("SQL");
+            Logger.log(sqlId + ": " + query);
+        }
+
+        tx.executeSql(query, args, function (tx, results) {
+
+            // Load all rows in memory
+            var rows = [];
+            var len = results.rows.length, i;
+            for (i = 0; i < len; i++) {
+                rows.push(_.clone(results.rows.item(i)));
+            }
+
+            // Log success of the request
+            if (loggingEnabled && log !== false) {
+                Logger.log(sqlId + ": ok: " +
+                            (rows.length ? (rows.length + " rows") : ""));
+            }
+
+            // Callback
+            if (typeof success === "function") {
+                success.call(context, rows);
+            }
+        },
+        function (tx, err) {
+            // Some logs for the user.
+            if (log !== false) {
+                if (Database.loggingEnabled) {
+                    Logger.log(sqlId + ": error");
+                }
+                Logger.log("SQL ERROR: " + err.message);
+                Logger.log("...query: " + query);
+                Logger.log("...args: " + JSON.stringify(args));
+            }
+
+            // Callback
+            if (typeof error === "function") {
+                error.call(context, err.message);
+            }
+        });
+    };
+
+    var webSQL_execTransaction = function (requests) {
+        Database.db.transaction(function (tx) {
+            for (var i = 0, l = requests.length; i < l; ++i) {
+                var r = requests[i];
+                webSQL_executeSql(tx, r.query, r.args, r.success, r.error, undefined, r);
+            }
+        });
+    };
+    var webSQL_exec = function (query, args, success, error, log) {
+        Database.db.transaction(function (tx) {
+            webSQL_executeSql(tx, query, args, success, error, log, this);
+        });
+    };
+
+    var plugin_exec = function (query, args, success, error, log) {
+        // Log the request
+        var sqlId,
+            loggingEnabled = Database.loggingEnabled;
+        if (loggingEnabled && log !== false) {
+            sqlId = _.uniqueId("SQL");
+            Logger.log(sqlId + ": " + query);
+        }
+
+        // Start profiling the request.
+        var timerId,
+            profilingEnabled = Database.profilingEnabled;
+        if (profilingEnabled && log !== false) {
+            timerId = Jackbone.profiler.onStart();
+        }
+
+        // Launch it in synchronous mode
+        Database.db.executeSql(query, args, function (results) { /*Now*/
+            var rows = results.rows;
+
+            if (log !== false) {
+                // Inform profiler that request is done.
+                if (profilingEnabled) {
+                    Jackbone.profiler.onEnd(timerId, query);
+                }
+
+                // Log
+                if (loggingEnabled) {
+                    Logger.log(sqlId + ": ok: " +
+                                (rows.length ? (rows.length + " rows") : ""));
+                }
+            }
+
+            // Callback
+            if (typeof success === "function") {
+                success(rows);
+            }
+        },
+        function (err) {
+            // Some logs for the user.
+            if (log !== false) {
+                if (Database.loggingEnabled) {
+                    Logger.log(sqlId + ": error");
+                }
+                Logger.log("SQL ERROR: " + err.message);
+                Logger.log("...query: " + query);
+                Logger.log("...args: " + JSON.stringify(args));
+
+                // Inform profiler that request is done (even if it failed).
+                if (profilingEnabled) {
+                    Jackbone.profiler.onEnd(timerId, query);
+                }
+            }
+            else {
+                console.log("SQL ERROR: " + err.message);
+                console.log("...query: " + query);
+                console.log("...args: " + JSON.stringify(args));
+            }
+
+            // Callback
+            if (typeof error === "function") {
+                error(err.message);
+            }
+        });
+    };
+
     /** Execute an SQL query on the database
      *
      * @param query SQL query string.
@@ -68,124 +197,14 @@ function (Logger, _, Events, Jackbone/*, SQLite*/) {
      * @return JSON output.
      */
     if (window.sqlitePlugin) {
-
-        Database.exec = function (query, args, success, error, log) {
-
-            // Log the request
-            var sqlId,
-                loggingEnabled = Database.loggingEnabled;
-            if (loggingEnabled && log !== false) {
-                sqlId = _.uniqueId("SQL");
-                Logger.log(sqlId + ": " + query);
-            }
-
-            // Start profiling the request.
-            var timerId,
-                profilingEnabled = Database.profilingEnabled;
-            if (profilingEnabled && log !== false) {
-                timerId = Jackbone.profiler.onStart();
-            }
-
-            // Launch it in synchronous mode
-            this.db.executeSql(query, args, function (results) { /*Now*/
-                var rows = results.rows;
-
-                if (log !== false) {
-                    // Inform profiler that request is done.
-                    if (profilingEnabled) {
-                        Jackbone.profiler.onEnd(timerId, query);
-                    }
-
-                    // Log
-                    if (loggingEnabled) {
-                        Logger.log(sqlId + ": ok: " +
-                                    (rows.length ? (rows.length + " rows") : ""));
-                    }
-                }
-
-                // Callback
-                if (typeof success === "function") {
-                    success(rows);
-                }
-            },
-            function (err) {
-                // Some logs for the user.
-                if (log !== false) {
-                    if (Database.loggingEnabled) {
-                        Logger.log(sqlId + ": error");
-                    }
-                    Logger.log("SQL ERROR: " + err.message);
-                    Logger.log("...query: " + query);
-                    Logger.log("...args: " + JSON.stringify(args));
-
-                    // Inform profiler that request is done (even if it failed).
-                    if (profilingEnabled) {
-                        Jackbone.profiler.onEnd(timerId, query);
-                    }
-                }
-                else {
-                    console.log("SQL ERROR: " + err.message);
-                    console.log("...query: " + query);
-                    console.log("...args: " + JSON.stringify(args));
-                }
-
-                // Callback
-                if (typeof error === "function") {
-                    error(err.message);
-                }
-            });
-        };
+        Database.exec = plugin_exec;
+        Database.execTransaction = webSQL_execTransaction;
     }
     else {
-        Database.exec = function (query, args, success, error, log) {
-
-            // Log the request
-            var sqlId, loggingEnabled = Database.loggingEnabled;
-            if (loggingEnabled && log !== false) {
-                sqlId = _.uniqueId("SQL");
-                Logger.log(sqlId + ": " + query);
-            }
-
-            this.db.transaction(function (tx) {
-                tx.executeSql(query, args, function (tx, results) {
-
-                    // Load all rows in memory
-                    var rows = [];
-                    var len = results.rows.length, i;
-                    for (i = 0; i < len; i++) {
-                        rows.push(_.clone(results.rows.item(i)));
-                    }
-
-                    // Log success of the request
-                    if (loggingEnabled && log !== false) {
-                        Logger.log(sqlId + ": ok: " +
-                                   (rows.length ? (rows.length + " rows") : ""));
-                    }
-
-                    // Callback
-                    if (typeof success === "function") {
-                        success(rows);
-                    }
-                },
-                function (tx, err) {
-                    // Some logs for the user.
-                    if (log !== false) {
-                        if (Database.loggingEnabled) {
-                            Logger.log(sqlId + ": error");
-                        }
-                        Logger.log("SQL ERROR: " + err.message);
-                        Logger.log("...query: " + query);
-                        Logger.log("...args: " + JSON.stringify(args));
-                    }
-
-                    // Callback
-                    if (typeof error === "function") {
-                        error(err.message);
-                    }
-                });
-            });
-        };
+        Database.exec = webSQL_exec;
+        Database.execTransaction = webSQL_execTransaction;
     }
+
 
     /** Delete everything from the database.
      * @param callback Function called when the operation is done.
